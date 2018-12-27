12584c32ea00fc650fdd77d5a73c0de1946de53e
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-12-27 22:16:14 +0530

    created redux store

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -14,12 +14,12 @@ import PhotoGrid from './components/PhotoGrid'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
-      <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-          <IndexRoute component={PhotoGrid}></IndexRoute>
-          <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-      </Router>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
   )
 
 render(router, document.getElementById('root'));
